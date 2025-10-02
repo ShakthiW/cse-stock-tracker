cf1ed849d7e40e5e3257b7c285e9dfb85e82a387
ShakthiW <shakthi.20221274@iit.ac.lk>
2025-10-02 19:04:36 +0530

    Add FooterLink component to sign-up page for account navigation
    - Introduced a new FooterLink component to provide a link for users to sign in if they already have an account.
    - Updated the sign-up page to include the FooterLink, enhancing user navigation options.

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CountrySelectField } from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
 import InputField from "@/components/forms/InputField";
 import SelectField from "@/components/forms/SelectField";
 import { Button } from "@/components/ui/button";
@@ -74,7 +75,7 @@ const SignUp = () => {
           error={errors.password}
           validation={{ required: "Password is required", minLength: 8 }}
         />
-        
+
         {/* Conuntry */}
         <CountrySelectField
           name="country"
@@ -121,6 +122,13 @@ const SignUp = () => {
             ? "Creating account..."
             : "Start your investment journey"}
         </Button>
+
+        {/* Footer Link */}
+        <FooterLink
+          text="Already have an account?"
+          linkText="Sign in"
+          href="/sign-in"
+        />
       </form>
     </>
   );

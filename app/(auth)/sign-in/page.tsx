97b03934b542356833660c5d5a3d6cf8b0be131d
ShakthiW <shakthi.20221274@iit.ac.lk>
2025-10-02 19:12:42 +0530

    Implement sign-in form with validation and submission handling
    - Added a sign-in form component with email and password fields using React Hook Form for validation.
    - Integrated InputField and Button components for user input and submission.
    - Included a FooterLink for navigation to the sign-up page, enhancing user experience.

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,80 @@
-import React from 'react'
+"use client";
+
+import FooterLink from "@/components/forms/FooterLink";
+import InputField from "@/components/forms/InputField";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onBlur",
+  });
 
-export default SignIn
+  const onSubmit = async (data: SignInFormData) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="form-title">Welcome back</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="Enter your email"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email is required",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Invalid email address",
+            },
+          }}
+        />
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter your password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{ required: "Password is required", minLength: 8 }}
+        />
+
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? "Signing in..." : "Sign in"}
+        </Button>
+
+        <FooterLink
+          text="Don't have an account?"
+          linkText="Create account"
+          href="/sign-up"
+        />
+      </form>
+    </>
+  );
+};
+
+export default SignIn;
